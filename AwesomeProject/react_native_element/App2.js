d2e2bd04b6b56a0c07b40762b7d4d36773eb9da3
HUYHOANG\huyhoang888 <awshuyhoang@gmail.com>
2017-06-17 20:55:15 +0700

    design

--- AwesomeProject/react_native_element/App2.js
+++ AwesomeProject/react_native_element/App2.js
@@ -7,18 +7,9 @@ import Screen_A from './Screen_A.js';
 import Screen_B from './Screen_B.js';
 import Screen_C from './Screen_C.js';
 import Screen_D from './Screen_D.js';
-import App2 from './App2.js';
 import RadarChartScreen from './RadarChartScreen.js';
-import SplashScreen from 'react-native-splash-screen';
 
-class App extends Component {
-
-    static navigationOptions = {
-        title: 'Luyện thi TOEIC',
-        headerTintColor: 'red',
-        headerRight: <Icon name='settings' color='#517fa4'
-            size={33} containerStyle={{ flex: 1, justifyContent: 'center', alignItems: 'center' }} />,
-    };
+class App2 extends Component {
 
     constructor() {
         super()
@@ -34,17 +25,14 @@ class App extends Component {
         });
     }
 
-    changeTab(selectedTab) {
+    changeTab2(selectedTab) {
         console.log('selectedTab');
         this.setState({ selectedTab })
         // navigate('Test_Screens');
     }
 
-    componentDidMount() {
-        SplashScreen.hide();
-    }
-
     render() {
+        console.log('chayngu');
         const { selectedTab } = this.state;
         let tabBarStyle = {};
         let sceneStyle = {};
@@ -54,7 +42,6 @@ class App extends Component {
             sceneStyle.paddingBottom = 0;
         }
 
-        const { navigate } = this.props.navigation;
         const buttons = ['Listening', 'Reading'];
         const { selectedIndex } = this.state;
 
@@ -120,30 +107,25 @@ class App extends Component {
                 <Tab
                     titleStyle={{ fontWeight: 'bold', fontSize: 10 }}
                     selectedTitleStyle={{ marginTop: -1, marginBottom: 6 }}
-                    selected={selectedTab === 'manhinhA'}
-                    title={selectedTab === 'manhinhA' ? 'Đánh giá' : null}
+                    selected={selectedTab === 'manhinhA2'}
+                    title={selectedTab === 'manhinhA2' ? 'Đánh giá' : null}
                     renderIcon={() => <Icon containerStyle={{ justifyContent: 'center', alignItems: 'center', marginTop: 12 }} color={'#5e6977'} name='create' size={33} />}
                     renderSelectedIcon={() => <Icon color={'#6296f9'} name='create' size={30} />}
-                    onPress={() => this.changeTab('manhinhA')}>
+                    onPress={() => this.changeTab2('manhinhA2')}>
 
                     <RadarChartScreen />
                 </Tab>
                 <Tab
                     titleStyle={{ fontWeight: 'bold', fontSize: 10 }}
                     selectedTitleStyle={{ marginTop: -1, marginBottom: 6 }}
-                    selected={selectedTab === 'manhinhB'}
-                    title={selectedTab === 'manhinhB' ? 'Từ Vựng' : null}
+                    selected={selectedTab === 'manhinhB2'}
+                    title={selectedTab === 'manhinhB2' ? 'Từ Vựng' : null}
                     renderIcon={() => <Icon containerStyle={{ justifyContent: 'center', alignItems: 'center', marginTop: 12 }} color={'#5e6977'} name='device-hub' size={33} />}
                     renderSelectedIcon={() => <Icon color={'#6296f9'} name='device-hub' size={30} />}
-                    onPress={() => this.changeTab('manhinhB')}>
-                    <View>
-                        <ButtonGroup
-                            onPress={this.updateIndex}
-                            selectedIndex={selectedIndex}
-                            buttons={buttons}
-                            containerStyle={{ height: 20 }} />
+                    onPress={() => this.changeTab2('manhinhB2')}>
 
-                        <List containerStyle={{ marginTop: 20, borderTopWidth: 10, borderBottomWidth: 1, borderBottomColor: 'red' }}>
+                    <View>
+                        <List containerStyle={{ marginTop: 0, borderTopWidth: 10, borderBottomWidth: 1, borderBottomColor: 'red' }}>
                             {
                                 list.map((item, i) => (
                                     <ListItem
@@ -166,79 +148,15 @@ class App extends Component {
                         </List>
                     </View>
                 </Tab>
-                <Tab
-                    titleStyle={{ fontWeight: 'bold', fontSize: 10 }}
-                    selectedTitleStyle={{ marginTop: -1, marginBottom: 6 }}
-                    selected={selectedTab === 'manhinhC'}
-                    title={selectedTab === 'manhinhC' ? 'Ngữ Pháp' : null}
-                    renderIcon={() => <Icon containerStyle={{ justifyContent: 'center', alignItems: 'center', marginTop: 12 }} color={'#5e6977'} name='content-paste' size={33} />}
-                    renderSelectedIcon={() => <Icon color={'#6296f9'} name='content-paste' size={30} />}
-                    onPress={() => this.changeTab('manhinhC')}>
 
-                    <App2 />
 
-                </Tab>
 
-                <Tab
-                    titleStyle={{ fontWeight: 'bold', fontSize: 10 }}
-                    selectedTitleStyle={{ marginTop: -1, marginBottom: 6 }}
-                    selected={selectedTab === 'manhinhD'}
-                    title={selectedTab === 'manhinhD' ? 'Luyện tập' : null}
-                    renderIcon={() => <Icon containerStyle={{ justifyContent: 'center', alignItems: 'center', marginTop: 12 }} color={'#5e6977'} name='headset' size={33} />}
-                    renderSelectedIcon={() => <Icon color={'#6296f9'} name='headset' size={30} />}
-                    onPress={() => this.changeTab('manhinhD')}>
-
-                    <View>
-                        <ScrollView horizontal={true}>
-                            <Button
-                                icon={{ name: 'cached' }}
-                                title='Reading' />
-                            <Button
-                                icon={{ name: 'cached' }}
-                                title='Listening' />
-                            <Button
-                                icon={{ name: 'cached' }}
-                                title='Reading' />
-                            <Button
-                                icon={{ name: 'cached' }}
-                                title='Listening' />
-                        </ScrollView>
-                        <ScrollView>
-                            <List containerStyle={{ marginTop: 0,marginBottom: 55, borderTopWidth: 0, borderBottomWidth: 0, borderBottomColor: 'red' }}>
-                                {
-                                    list.map((item, i) => (
-                                        <ListItem
-                                            roundAvatar
-                                            key={i}
-                                            title={item.name}
-                                            badge={{ value: 3, badgeTextStyle: { color: 'orange' }, badgeContainerStyle: { marginTop: -20 } }}
-                                            subtitle={
-                                                <Rating type="heart"
-                                                    readonly
-                                                    fractions={5}
-                                                    startingValue={4}
-                                                    imageSize={25}
-                                                />}
-                                            avatar={{ uri: item.avatar_url }}
-                                            onPress={() => navigate('Test_Screens')}
-                                        />
-                                    ))
-                                }
-                            </List>
-                        </ScrollView>
-                    </View>
-                </Tab>
             </Tabs >
         )
     }
+
+
+
 }
 
-// class  ManHinhA extends Component {
-//     render() {
-//         return(
-            
-//         )
-//     }
-// }
-
-export default App;
+export default App2;
