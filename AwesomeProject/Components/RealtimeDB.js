b46d2c22410d4822bb8538f9243c0fab1d94fc9f
HUYHOANG\huyhoang888 <awshuyhoang@gmail.com>
2017-06-18 17:53:35 +0700

    Add data with textinput

--- AwesomeProject/Components/RealtimeDB.js
+++ AwesomeProject/Components/RealtimeDB.js
@@ -3,6 +3,7 @@ import {
     Text,
     View,
     TouchableOpacity,
+    TextInput
 } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 import { firebaseApp } from './FirebaseConfig.js';
@@ -13,6 +14,9 @@ export default class RealtimeDB extends Component {
     constructor(props) {
         super(props);
         this.itemRef = firebaseApp.database();
+        this.state = {
+            text: ''
+        }
     }
 
     setDB() {
@@ -39,6 +43,28 @@ export default class RealtimeDB extends Component {
         })
     }
 
+    //lientuc cap nhat,kich hoat lai event
+    onDB() {
+        this.itemRef.ref('KhoaHoc').child('Android').on('value', function (snapshot) {
+            alert(snapshot.val())
+        })
+    }
+    //ko kich hoat lai event
+    onceDB() {
+        this.itemRef.ref('KhoaHoc').child('Android').once('value', function (snapshot) {
+            alert(snapshot.val())
+        })
+    }
+
+    nhapdulieu() {
+        this.itemRef.ref('ChatRoom').child('PhongChat').push({
+            LapTrinh: this.state.text
+        });
+        this.setState({
+            text: ''
+        })
+    }
+
     render() {
         const { navigate } = this.props.navigation;
         return (
@@ -54,9 +80,28 @@ export default class RealtimeDB extends Component {
                     <Text style={{ color: 'blue', fontSize: 40 }}>Push DB</Text>
                 </TouchableOpacity>
 
-                 <TouchableOpacity style={{ backgroundColor: 'red' }}
+                <TouchableOpacity style={{ backgroundColor: 'red' }}
                     onPress={() => { this.childDB() }} >
                     <Text style={{ color: 'blue', fontSize: 40 }}>Child DB</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={{ backgroundColor: 'red' }}
+                    onPress={() => { this.onDB() }} >
+                    <Text style={{ color: 'blue', fontSize: 40 }}>On DB</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={{ backgroundColor: 'red' }}
+                    onPress={() => { this.onceDB() }} >
+                    <Text style={{ color: 'blue', fontSize: 40 }}>Once DB</Text>
+                </TouchableOpacity>
+
+                <TextInput
+                    style={{ height: 40, width: 300, borderColor: 'gray', borderWidth: 1 }}
+                    onChangeText={(text) => this.setState({ text })}
+                    value={this.state.text}
+                />
+                <TouchableOpacity style={{ backgroundColor: 'red' }}
+                    onPress={() => { this.nhapdulieu() }} >
+                    <Text style={{ color: 'blue', fontSize: 40 }}>Them du lieu</Text>
                 </TouchableOpacity>
             </View>
         );
